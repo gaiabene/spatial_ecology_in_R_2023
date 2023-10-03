bdf2479d04e5077cb52e88fc67196e81945ed7ac
gaiabene <146854520+gaiabene@users.noreply.github.com>
2023-10-03 17:52:24 +0200

    Update 01.beginning.R

--- code/01.beginning.R
+++ code/01.beginning.R
@@ -9,5 +9,19 @@ duccio * zima
 final <- duccio * zima
 final
 
+# array
+sophi <- c(10, 20, 30, 50, 70) #microplastics #numbers are called arguments
+#functions have parentheses and inside theme there are arguments
 
+paula <- c(100, 500, 600, 1000, 2000) #people
 
+plot(paula, sophi)
+
+plot(paula, sophi, xlab="number of people", ylab="microplastics")
+
+people <- paula 
+microplastics <- sophi
+
+plot(people, microplastics, pch=19)
+
+#PCH

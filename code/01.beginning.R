cb9bb4abd5ff02bf15a92cb9375a4000461cd840
gaiabene <146854520+gaiabene@users.noreply.github.com>
2023-11-30 16:01:10 +0100

    01.beginning.R

--- code/01.beginning.R
+++ code/01.beginning.R
@@ -1,27 +1,36 @@
 #R as a calculator 
+5+4
+
+#assign to an object
 zima <- 2+3
 zima
 
 duccio <- 1 + 6
 duccio
 
+#computations 
 duccio * zima
 final <- duccio * zima
 final
 
-# array
-sophi <- c(10, 20, 30, 50, 70) #microplastics #numbers are called arguments
-#functions have parentheses and inside theme there are arguments
 
-paula <- c(100, 500, 600, 1000, 2000) #people
 
+####Array
+sophi <- c(10, 20, 30, 50, 70)  #microplastics 
+
+#numbers are called arguments
+#functions have parentheses and inside them there are arguments
+
+paula <- c(100, 500, 600, 1000, 2000)  #people
+
+#we can plot them together 
 plot(paula, sophi)
-
 plot(paula, sophi, xlab="number of people", ylab="microplastics")
 
+#we can also do like that
 people <- paula 
 microplastics <- sophi
 
 plot(people, microplastics, pch=19)
 
-#PCH
+#http://www.sthda.com/english/

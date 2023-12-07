01ed87d934c195be995f739e2ad084dba8b3a2a5
gaiabene <146854520+gaiabene@users.noreply.github.com>
2023-12-07 16:47:02 +0100

    Update 10.classification.R

--- code/10.classification.R
+++ code/10.classification.R
@@ -8,10 +8,14 @@ library(imageRy)
 
 im.list()
 
-# we use 29 Solar to classify the solar energy based on gasses 
+# we use number 29: Solar_Orbiter to classify the solar energy based on gasses 
 # https://www.esa.int/ESA_Multimedia/Images/2020/07/Solar_Orbiter_s_first_views_of_the_Sun6
 
 sun <- im.import ("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 # we have to explain to the software the clusters 
 # here we have 3 clusters 
+
+sunc <- im.classify(sun, num_clusters=3)
+plot(sunc)
+# in this case the part with the higher energy is the green one 
 

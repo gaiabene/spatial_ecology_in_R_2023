1197e6f9c1a455cf1245ba3a3b8edcd46382742b
gaiabene <146854520+gaiabene@users.noreply.github.com>
2023-12-07 17:30:05 +0100

    Update 10.classification.R

--- code/10.classification.R
+++ code/10.classification.R
@@ -21,15 +21,61 @@ plot(sunc)
 
 im.list()
 
-# import matogrosso 
-m1992 <- im.import("matogrosso_ast_2006209_lrg.jpg")
-m2006 <- im.import("matogrosso_l5_1992219_lrg.jpg")   
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")   
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+
 
 plotRGB(m1992)
+#here water should be black but there are several deposits so soil 
+#we use 2 clusters: one for forest and one for soil, agriculture  etc..
 
-# we use two clusters: one for forest and one for soil, agriculture etc 
 m1992c <- im.classify(m1992, num_clusters = 2)
 plot(m1992c)
-# classes: forest=1, human=2
+#classes: forest=1, human=2
+
+plotRGB(m2006)
+m2006c <- im.classify(m2006, num_clusters = 2)
+plot(m2006c)
+#classes: forest=1, human=2
+
+par(mfrow=c(1,2))
+plot(m1992c[[1]])
+plot(m2006c[[1]])
+
+f1992 <- freq(m1992c)
+
+f1992
+
+# let's extract the total number of pixels 
+tot1992 <- ncell(m1992c)
+tot1992
+
+# let's calculate the percentage by dividing by the total number of pixels 
+p1992 <- f1992 * 100 / tot1992
+p1992
+
+#forest:83%; human: 17%
+
+
+#percentage of 2006
+f2006 <- freq(m2006c)
+f2006
+
+tot2006 <- ncell(m2006c)
+tot2006
+
+p2006 <- f2006 * 100 / tot2006
+p2006
+
+# forest: 45%; human: 55%
+
+# bulding the final table
+# first of all we build the colums 
+
+class <- c("forest", "human")
+y1992 <- c(83, 17)
+y2006 <- c(45, 55)
+
+
 
 
